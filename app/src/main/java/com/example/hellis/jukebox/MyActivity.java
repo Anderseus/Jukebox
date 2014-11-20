3a10e6a284860cc01da466c34a135b38677ad44c
sweedfsh <henrysp3@gmail.com>
2014-11-20 13:09:18 -0500

    user authentication

--- app/src/main/java/com/example/hellis/jukebox/MyActivity.java
+++ app/src/main/java/com/example/hellis/jukebox/MyActivity.java
@@ -1,36 +1,118 @@
+// TutorialApp
+// Created by Spotify on 25/02/14.
+// Copyright (c) 2014 Spotify. All rights reserved.
 package com.example.hellis.jukebox;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
 
+import com.spotify.sdk.android.Spotify;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.authentication.SpotifyAuthentication;
+import com.spotify.sdk.android.playback.ConnectionStateCallback;
+import com.spotify.sdk.android.playback.Player;
+import com.spotify.sdk.android.playback.PlayerNotificationCallback;
+import com.spotify.sdk.android.playback.PlayerState;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements
+        PlayerNotificationCallback, ConnectionStateCallback {
+
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "2ac2d3abf554449191f22390ccc227df";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "jukebox://callback";
+
+    private Player mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        SpotifyAuthentication.openAuthWindow(CLIENT_ID, "token", REDIRECT_URI,
+                new String[]{"user-read-private", "streaming"}, null, this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        if (uri != null) {
+            AuthenticationResponse response = SpotifyAuthentication.parseOauthResponse(uri);
+            Spotify spotify = new Spotify(response.getAccessToken());
+            mPlayer = spotify.getPlayer(this, "My Company Name", this, new Player.InitializationObserver() {
+                @Override
+                public void onInitialized() {
+                    mPlayer.addConnectionStateCallback(MyActivity.this);
+                    mPlayer.addPlayerNotificationCallback(MyActivity.this);
+                    mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    Log.e("MyActivity", "Could not initialize player: " + throwable.getMessage());
+                }
+            });
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MyActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MyActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MyActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MyActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onNewCredentials(String s) {
+        Log.d("MyActivity", "User credentials blob received");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MyActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("MyActivity", "Playback event received: " + eventType.name());
+        switch (eventType) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("MyActivity", "Playback error received: " + errorType.name());
+        switch (errorType) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        // VERY IMPORTANT! This must always be called or else you will leak resources
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
     }
 }
